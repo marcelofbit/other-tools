2099c30d82e7562c17249281a84784f6a23f86a5
marcelofbit <103432309+marcelofbit@users.noreply.github.com>
2024-06-10 17:03:32 +0200

    Update fn-install-docker-all-os.sh
    Add Docker-compose install.
    
    Signed-off-by: marcelofbit <103432309+marcelofbit@users.noreply.github.com>

--- fn-install-docker-all-os.sh
+++ fn-install-docker-all-os.sh
@@ -4,3 +4,6 @@ sudo usermod -aG docker $USER
 sudo systemctl start docker
 sudo systemctl enable docker
 docker --version
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.14.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version

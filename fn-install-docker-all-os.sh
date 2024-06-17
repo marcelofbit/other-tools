c2cffa911d8ba4b429e673160427fabf8707e79f
marcelofbit <103432309+marcelofbit@users.noreply.github.com>
2024-06-17 12:47:58 +0200

    Update fn-install-docker-all-os.sh
    Signed-off-by: marcelofbit <103432309+marcelofbit@users.noreply.github.com>

--- fn-install-docker-all-os.sh
+++ fn-install-docker-all-os.sh
@@ -1,8 +1,8 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo usermod -aG docker $USER
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo usermod -aG docker $USER
 docker --version
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.14.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose

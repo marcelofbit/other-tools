b5d3719d4601ff253925550bc80d420ae6b40108
marcelofbit <103432309+marcelofbit@users.noreply.github.com>
2024-07-03 10:38:40 +0200

    Update fn-install-docker-all-os.sh
    Update version Docker Compose 2.28.1
    
    Signed-off-by: marcelofbit <103432309+marcelofbit@users.noreply.github.com>

--- fn-install-docker-all-os.sh
+++ fn-install-docker-all-os.sh
@@ -4,6 +4,6 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
 docker --version
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.14.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version

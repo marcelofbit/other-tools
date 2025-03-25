f3ad79d42b62611c747182b6877e17f9ab094254
marcelofbit <103432309+marcelofbit@users.noreply.github.com>
2025-03-25 09:20:36 +0100

    Update fn-init0.sh
    Signed-off-by: marcelofbit <103432309+marcelofbit@users.noreply.github.com>

--- fn-init0.sh
+++ fn-init0.sh
@@ -18,6 +18,8 @@ echo "                                                 ╚════╝    █
 echo "                                                           ██║     ██║ ╚████║          "
 echo "                                                           ╚═╝     ╚═╝  ╚═══╝          Instalando pacotes adicionais..."
 echo ""
+apt update -y
+apt install sudo curl -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo systemctl start docker
